c8d8095a292b2eda9ac55eba39fe36bad1ae7641
Gabrielle Nyirjesy <57142038+gnyirjesy@users.noreply.github.com>
2022-11-14 15:31:44 -0500

    Cluster classification gn (#29)
    * add common functions in scripts
    
    * classification model of Nick's clusters
    
    * import necessary packages
    
    * split normalized data into train/test
    
    * small update to code
    
    * finalize cluster classificaiton code

---
+++ utils/plotting.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import numpy as np
+
+def plot_on_map(df, source, features=None, data_type=None, figsize=(10,4), subtitle=None):
+    '''
+    function to plot latitude and longitude of (float or satellite) data over the world map, colored by zero or more features
+    
+    arguments
+    ---------
+    df (pandas df):
+        dataframe, already filtered as desired
+    source (str):
+            satellite, float, or a matchup; used in the plot titles
+    features (str or list-like or None):
+              string of a single feature, or list of strings of features corresponding to column names of df;
+              there will be a different plot for every feature;
+              if features is None, just latitude and longitude will be plotted with no coloring by feature;
+              list of features may include None
+    data_type (str or list-like or None):
+               single or list containing 'continuous', 'discrete' or 'categorical', or None to determine use of colorbar, legend, or no color respectively ;
+               if a single value, then it is assumed to be applied to all features;
+               if a list, then it it must have the same length as the list of features
+    figsize (tuple):
+             figsize argument passed to plt.figure()
+    subtitle (str or None):
+              optional additional line to add to the title; a single string that will be applied to all plots
+    '''
+    
+    # argument modifications
+    if type(features) == str:
+        features = [features]
+        
+    if features is None:
+        features = [None]
+        
+    if data_type == 'discrete':
+        data_type = 'categorical'
+    
+    if type(data_type) == list:
+        data_type = ['categorical' if t=='discrete' else t for t in data_type]
+        
+    if type(data_type) == list and len(features) != len(data_type):
+        print('Error — len(features) and len(data_type) should be equal')
+        return
+    
+    if subtitle is not None and subtitle.strip() == '':
+        subtitle = None
+        
+    # obtain map
+    land = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+    
+    for f in features:
+        
+        # plot the base map
+        land.plot(color='lightgray', figsize=figsize)
+        
+        # scatter plot with no color if feature is None
+        if f is None:
+            plt.scatter(df['longitude'], df['latitude'], s=3, alpha=0.7)
+            if subtitle is None:
+                plt.title(f'Latitude vs. Longitude for {source.title()} Data', fontsize='x-large')
+            else:
+                plt.title(f'Latitude vs. Longitude for {source.title()} Data\n({subtitle})', fontsize='x-large')
+            
+        else:
+            f_df = df[df[f].notna()].reset_index(drop=True)
+
+            # scatter plot, colored by continuous feature
+            if data_type == 'continuous' or (type(data_type) == list and data_type[features.index(f)] == 'continuous'):
+                plt.scatter(f_df['longitude'], f_df['latitude'], c=f_df[f], cmap='plasma', s=3, alpha=0.7)
+                plt.colorbar(label=f)
+
+            # scatter plot, colored by categorical feature
+            elif data_type == 'categorical' or (type(data_type) == list and data_type[features.index(f)] == 'categorical'):
+                colors =  [list(np.unique(f_df[f])).index(v) for v in f_df[f]]
+                color_indices = np.unique(colors, return_index=True)[1]
+                labels = [f_df.loc[i,f] for i in sorted(color_indices)]
+                scatter = plt.scatter(f_df['longitude'], f_df['latitude'], c=colors, cmap='plasma', s=3, alpha=0.7)
+                plt.legend(handles=scatter.legend_elements()[0], labels=labels, title=f, loc='upper left', bbox_to_anchor=(1,1))
+
+            if subtitle is None:
+                plt.title(f'{source.title()} Data Latitude vs. Longitude\nColored by {f}', fontsize='x-large')
+            else:
+                plt.title(f'{source.title()} Data Latitude vs. Longitude\nColored by {f}\n({subtitle})', fontsize='x-large')
+            
+        plt.xlabel('longitude')
+        plt.ylabel('latitude')
+        
+        # display plot
+        plt.show()
