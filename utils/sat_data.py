03a83b049ee1a4424f8c8ee661c37812a6e572b0
Gabrielle Nyirjesy <57142038+gnyirjesy@users.noreply.github.com>
2022-11-16 18:37:40 -0500

    add global data query option for satellite pull (#33)

--- utils/sat_data.py
+++ utils/sat_data.py
@@ -2,15 +2,26 @@
 """
 Functions to assist with satellite data query and processing
 Use a virtual environment to run this code if running alone. From your computer command line enter the following commands:
-1. conda create -n oceandata_pull python==3.10.6 numpy==1.23.3 pandas==1.5.0 xarray==2022.9.0 requests==2.28.1
-2. conda activate oceandata_pull
-3. conda install -c conda-forge netcdf4
-Now you have a virtual environment that will work with this script. You can pull the CHL data for a location 
-at latitude 40, longitude -60, date 2/4/2022 usine the following command line code by substiting the appkey_val
-with your NASA Earthdata app_key:
 
-python sat_data.py 40 -60 2022 2 4 'CHL.chlor_a' "appkey_val"
+    1. conda create -n oceandata_pull python==3.10.6 numpy==1.23.3 pandas==1.5.0 xarray==2022.9.0 requests==2.28.1
+    2. conda activate oceandata_pull
+    3. conda install -c conda-forge netcdf4
 
+Single location Query:
+    Now you have a virtual environment that will work with this script. You can pull the CHL data for a location 
+    at latitude 40, longitude -60, date 2/4/2022 usine the following command line code by substiting the appkey_val
+    with your NASA Earthdata app_key:
+
+        python sat_data.py 2022 2 4 'CHL.chlor_a' "appkey_val" --lat 40 --lon -60
+
+    The output of this command will be a single number which is the [chla] value at (40,60) on 2/4/2022 (imputed with day/8-day/month procedure)
+
+Global Query:
+    If you want to pull the global data for a specific date (2/4/2022) use the following command:
+
+        python sat_data.py 2022 2 4 'CHL.chlor_a' "appkey_val"
+
+    The output of this command is a netcdf file of the [chla] on 2/4/2022 (imputed with day/8-day/month procedure)
 
 An appkey can be obtained from:
 #    https://oceandata.sci.gsfc.nasa.gov/appkey/
@@ -171,6 +182,7 @@ def day_8d_month_impute(latitude, longitude, lookup_date, var_oceandata, date_va
         var_oceandata: str, name of variable on oceandata
         date_values: list, list with all 8 day options
         appkey_val: str, appkey from oceandata
+        
 
     Outputs:
         float, satelitte value
@@ -183,16 +195,54 @@ def day_8d_month_impute(latitude, longitude, lookup_date, var_oceandata, date_va
             sat_val = get_sat_val(latitude, longitude, lookup_date, var_oceandata, 'month', date_values, appkey_val)
     return(sat_val)
 
+def day_8d_month_impute_global(lookup_date, var_oceandata, date_values, appkey_val, outputDir):
+    """
+     Description: Query and impute the satelitte data for a specific variable (first fill with
+     daily value, then 8-day, then monthly average)
+
+    Inputs:
+        lookup_date: datetime.date object, date of interest to pull in data for all locations
+        var_oceandata: str, name of variable on oceandata
+        date_values: list, list with all 8 day options
+        appkey_val: str, appkey from oceandata
+        outputDir: str, location to save output file
+
+    Outputs:
+        netcdf file
+    """
+    day_file = get_filename(lookup_date,date_values, var_oceandata,'day')
+    query_data(day_file, appkey_val,'/temp/')
+    if exists('./temp/'+day_file):
+        day = xr.open_dataset('./temp/'+day_file)
+        os.system("rm ./temp/" + day_file)
+        day8_file = get_filename(lookup_date,date_values, var_oceandata,'8d')
+        query_data(day8_file, appkey_val,'/temp/')
+        if exists('./temp/'+day8_file):
+            day8 = xr.open_dataset('./temp/'+day8_file)
+            day = day.fillna(day8)
+            os.system("rm ./temp/" + day8_file)
+        month_file = get_filename(lookup_date,date_values, var_oceandata,'month')
+        query_data(month_file, appkey_val,'/temp/')
+        if exists('./temp/'+month_file):
+            month = xr.open_dataset('./temp/'+month_file)
+            day = day.fillna(month)
+            os.system("rm ./temp/" + month_file)
+    else:
+        return(print('Error with data pull'))
+    day.to_netcdf(outputDir + var_oceandata.split('.')[1]+'_'+str(lookup_date.year)+str(lookup_date.month)+str(lookup_date.day)+'.nc')
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Pull satellite date for given latitude, longitude, date')
-    parser.add_argument('latitude',type=float, help='latitude of interest')
-    parser.add_argument('longitude',type=float, help='longitude of interest')
+    parser.add_argument('--latitude','--lat',default=None, type=float, help='latitude of interest')
+    parser.add_argument('--longitude','--lon',default = None,type=float, help='longitude of interest')
     parser.add_argument('year', type=int, help='year of interest')
     parser.add_argument('month', type=int, help='month of interest')
     parser.add_argument('day', type=int, help='day of interest')
     parser.add_argument('OceanDataVariable', type=str, help='variable of interest, options include CHL.chlor_a, FLH.ipar, etc.')
     parser.add_argument('appkey_val',type = str, help='NASA EarthData appkey')
+    parser.add_argument('--outputDir','--outputDir',default = './',type=str, help='location to save output netcdf file')
     args = parser.parse_args()
+    outputDir = args.outputDir
     latitude = args.latitude
     longitude = args.longitude
     year = args.year
@@ -208,6 +258,10 @@ if __name__ == '__main__':
     #Sort so the bisect works when used later
     date_values.sort()
     # print(get_filename(lookup_date, date_values, oceandata_var, 'day'))
-    results = day_8d_month_impute(latitude, longitude, lookup_date, oceandata_var, date_values, appkey_val)
-    print(results)
+    #If a latitude is given then find the single satellite value for specified location
+    if (latitude is not None)&(longitude is not None):
+        results = day_8d_month_impute(latitude, longitude, lookup_date, oceandata_var, date_values, appkey_val)
+        print(results)
+    else:
+        day_8d_month_impute_global(lookup_date, oceandata_var, date_values, appkey_val, outputDir)
     # results.to_csv('./results.csv')
